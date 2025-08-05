d85f7ae6b741a20b7d009434bcb43ad550fded74
Pringles <pedroworkpartner@outlook.com>
2025-08-05 20:45:50 -0300

    Modulo1

---
+++ Modulos/Modulo1/app2.py
@@ -0,0 +1,43 @@
+#TIPOS DE DADOS
+
+print (1+10+1000)
+print (1.5 + 2.5)
+print (True, False)
+print ('python')
+
+#dir()
+#help()
+
+#mutaveis - variaveis que podem ser alteradas
+age = 28 
+name = "guilherme"
+print (age, name)
+
+#Constantes - variaveis que nao podem ser alteradas devem ser escritas em maiusculo 
+PI = 3.14
+TOTAL = 1000
+print (PI, TOTAL)
+#SnakeCase - estilo de escrita de variaveis
+nome_minusculo = "guilherme"
+print (nome_minusculo)
+
+#CONVERTENDO TIPOS DE DADOS
+preco = 10
+print (preco)
+#convertido
+preco = float(preco)
+print(preco)
+#ou
+price = 5,4
+print(price)
+#convertido
+price = int(price)
+print(price)
+
+#Tem que converter numero em string para concatenar
+preco = 10
+nome = "guilherme"      
+print("O preco do produto e " + str(preco) + " e o nome do vendedor e " + nome)
+
+
+
